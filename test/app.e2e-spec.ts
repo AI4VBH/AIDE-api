d54d081a5a92cdaa9a02acfea8a76a0ec8d646f3
RemakingEden <joss.sparkes@gmail.com>
2022-09-05 15:42:32 +0100

    Added some semi pseudocode for how tests will work

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+require('./mocks/payloads');
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -16,33 +17,54 @@ describe('AppController (e2e)', () => {
   });
 
   describe('/payloads tests', () => {
+
+    jest.mock('./../src/app.module', () => ({
+      payloads: jest.fn()
+    }));
+
     it('GET /payloads with returned data', () => {
       //mock with data driven
+      test.each`
+        mock                        |   
+        ${"basic-payload-1.json"}   |
+        ${"basic-payload-2.json"}   |
+        ${"basic-payload-3.json"}   |`
+      AppModule.payloads.mockResolvedValue('$mock')
       return request(app.getHttpServer())
         .get('/payloads')
         .expect(200);
+        //expect how the data should be laid out
     });
   
     it('GET /payloads without returned data', () => {
+      AppModule.payloads.mockResolvedValue('empty-data.js')
       return request(app.getHttpServer())
       //mock empty response
         .get('/payloads')
-        .expect(200);
+        .expect(200)
+        .expect('"data":[]');
     });
   
     it('GET /payloads when Monai doesnt respond', () => {
+      AppModule.payloads.mockRejectedValueOnce(new Error('Connection Failed or something'))
       return request(app.getHttpServer())
             //mock 500 response
         .get('/payloads')
-        .expect(500)
+        .expect(400)
         .expect('Decent error message');
     });
 
     it('GET /payloads passes through pagination query', () => {
       // Make data driven
+      test.each`
+        paginationQuery                        |   
+        ${"?pageNumber=1&pageSize=10"}   |
+        ${"?pageNumber=10&pageSize=1"}   |
+        ${"?pageNumber=1&pageSize=5"}   |`
       return request(app.getHttpServer())
-        .get('/payloads?pageNumber=1&pageSize=10')
+        .get('/payloads' + '$paginationQuery')
         .expect(200);
+        //expect how the data should be laid out
     });
   });
 
