8b219c23957c11ffa5fb5c78f4cf2fa4d6662b57
Rowell Heria <rowellx68@users.noreply.github.com>
2022-10-24 16:07:31 +0100

    Add Helmet package to add default server headers (#75)
    Co-authored-by: Rowell Heria <rowell.heria@answerdigital.com>

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app: NestExpressApplication = await NestFactory.create(AppModule);
   const config: ConfigService = app.get(ConfigService);
   const port: number = config.get<number>('PORT');
 
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   await app.listen(port, () => {
